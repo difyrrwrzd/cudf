difyrrwrzd/cudf

commit 95272359b729735f46d90fb45b26b05c6c5e4032

Use optional-iterator for copy-if-else kernel (#9324)
This PR changes the `cudf::detail::copy_if_else` utility functions to accept an optional-iterator instead of a pair-iterator. This improves the compile time of source files by generating 4x less kernels since the two input data arrays can each have nulls requiring 4 different pair-iterators to be created to call it. The optional-iterator allows the nulls check to occur at runtime instead of compile time.

The changes in this PR are for the callers of `detail::copy_if_else` to provide optional-iterators instead of pair-iterators. This PR is dependent on the changes in #9306 

The benchmarks for the effected calling functions showed no significant change in runtime performance using the single optional-iterator over 4 unique pair-iterators. Two additional benchmarks are included cover non-null measurement which this PR impacts the most.

Also, the `copy_tests.cu` was renamed `copy_tests.cpp` and the test that launched to the internal  `cudf::detail::copy_if_else_kernel` was replaced with one with a data-set large enough to require multiple blocks.

Related changes for the strings specific `cudf::strings::detail::copy_if_else` are in #9266

Authors:
  - David Wendt (https://github.com/davidwendt)

Approvers:
  - Bradley Dice (https://github.com/bdice)
  - Robert Maynard (https://github.com/robertmaynard)
  - Jake Hemstad (https://github.com/jrhemstad)

URL: https://github.com/rapidsai/cudf/pull/9324

cpp/include/cudf/detail/copy_if_else.cuh
@@ -19,17 +19,10 @@
 #include <cudf/column/column.hpp>
 #include <cudf/column/column_device_view.cuh>
 #include <cudf/column/column_factories.hpp>
-#include <cudf/column/column_view.hpp>
 #include <cudf/detail/utilities/cuda.cuh>
 #include <cudf/detail/utilities/integer_utils.hpp>
-#include <cudf/scalar/scalar.hpp>
-#include <cudf/scalar/scalar_device_view.cuh>
-#include <cudf/utilities/traits.hpp>
-#include <cudf/utilities/type_dispatcher.hpp>
 
 #include <rmm/device_scalar.hpp>
-
-#include <cub/cub.cuh>
 
 namespace cudf {
 namespace detail {
@@ -40,7 +33,7 @@ template <size_type block_size,
           typename LeftIter,
           typename RightIter,
           typename Filter,
-          bool has_validity>
+          bool has_nulls>
 __launch_bounds__(block_size) __global__
   void copy_if_else_kernel(LeftIter lhs,
                            RightIter rhs,
@@ -71,23 +64,14 @@ __launch_bounds__(block_size) __global__
   size_type warp_cur = warp_begin + warp_id;
   size_type index    = tid;
   while (warp_cur <= warp_end) {
-    bool in_range = (index >= begin && index < end);
-
-    bool valid = true;
-    if (has_validity) {
-      valid = in_range && (filter(index) ? thrust::get<1>(lhs[index]) : thrust::get<1>(rhs[index]));
-    }
-
-    // do the copy if-else
-    if (in_range) {
-      out.element<T>(index) = filter(index) ? static_cast<T>(thrust::get<0>(lhs[index]))
-                                            : static_cast<T>(thrust::get<0>(rhs[index]));
-    }
+    auto const opt_value =
+      (index < end) ? (filter(index) ? lhs[index] : rhs[index]) : thrust::nullopt;
+    if (not has_nulls or opt_value) { out.element<T>(index) = static_cast<T>(*opt_value); }
 
     // update validity
-    if (has_validity) {
+    if (has_nulls) {
       // the final validity mask for this warp
-      int warp_mask = __ballot_sync(0xFFFF'FFFF, valid && in_range);
+      int warp_mask = __ballot_sync(0xFFFF'FFFF, opt_value.has_value());
       // only one guy in the warp needs to update the mask and count
       if (lane_id == 0) {
         out.set_mask_word(warp_cur, warp_mask);
@@ -100,7 +84,7 @@ __launch_bounds__(block_size) __global__
     index += block_size * gridDim.x;
   }
 
-  if (has_validity) {
+  if (has_nulls) {
     // sum all null counts across all warps
     size_type block_valid_count =
       single_lane_block_sum_reduce<block_size, leader_lane>(warp_valid_count);
@@ -168,8 +152,8 @@ std::unique_ptr<column> copy_if_else(
   rmm::cuda_stream_view stream,
   rmm::mr::device_memory_resource* mr = rmm::mr::get_current_device_resource())
 {
-  using Element =
-    typename thrust::tuple_element<0, typename thrust::iterator_traits<LeftIter>::value_type>::type;
+  // This is the type of the thrust::optional element in the passed iterators
+  using Element = typename thrust::iterator_traits<LeftIter>::value_type::value_type;
 
   size_type size           = std::distance(lhs_begin, lhs_end);
   size_type num_els        = cudf::util::round_up_safe(size, warp_size);

cpp/benchmarks/CMakeLists.txt
@@ -89,6 +89,10 @@ ConfigureBench(CONTIGUOUS_SPLIT_BENCH copying/contiguous_split_benchmark.cu)
 ConfigureBench(SHIFT_BENCH copying/shift_benchmark.cu)
 
 ###################################################################################################
+# - copy-if-else benchmark -----------------------------------------------------------------------------
+ConfigureBench(COPY_IF_ELSE_BENCH copying/copy_if_else_benchmark.cpp)
+
+###################################################################################################
 # - transpose benchmark ---------------------------------------------------------------------------
 ConfigureBench(TRANSPOSE_BENCH transpose/transpose_benchmark.cu)
 
@@ -141,7 +145,8 @@ ConfigureBench(REDUCTION_BENCH
 ###################################################################################################
 # - reduction benchmark ---------------------------------------------------------------------------
 ConfigureBench(REPLACE_BENCH
-  replace/clamp_benchmark.cpp)
+  replace/clamp_benchmark.cpp
+  replace/nans_benchmark.cpp)
 
 ###################################################################################################
 # - filling benchmark -----------------------------------------------------------------------------

cpp/benchmarks/copying/copy_if_else_benchmark.cpp
@@ -1,0 +1,67 @@
+/*
+ * Copyright (c) 2021, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <benchmark/benchmark.h>
+#include <benchmarks/common/generate_benchmark_input.hpp>
+#include <benchmarks/fixture/benchmark_fixture.hpp>
+#include <benchmarks/synchronization/synchronization.hpp>
+
+#include <cudf/copying.hpp>
+
+#include <rmm/device_buffer.hpp>
+
+class CopyIfElse : public cudf::benchmark {
+};
+
+template <class TypeParam>
+static void BM_copy_if_else(benchmark::State& state, bool nulls)
+{
+  cudf::size_type const n_rows{(cudf::size_type)state.range(0)};
+  auto input_type  = cudf::type_to_id<TypeParam>();
+  auto bool_type   = cudf::type_id::BOOL8;
+  auto const input = create_random_table({input_type, input_type, bool_type}, 3, row_count{n_rows});
+
+  if (!nulls) {
+    input->get_column(2).set_null_mask(rmm::device_buffer{}, 0);
+    input->get_column(1).set_null_mask(rmm::device_buffer{}, 0);
+    input->get_column(0).set_null_mask(rmm::device_buffer{}, 0);
+  }
+
+  cudf::column_view decision(input->view().column(2));
+  cudf::column_view rhs(input->view().column(1));
+  cudf::column_view lhs(input->view().column(0));
+
+  for (auto _ : state) {
+    cuda_event_timer raii(state, true, rmm::cuda_stream_default);
+    cudf::copy_if_else(lhs, rhs, decision);
+  }
+}
+
+#define COPY_BENCHMARK_DEFINE(name, type, b)                  \
+  BENCHMARK_DEFINE_F(CopyIfElse, name)                        \
+  (::benchmark::State & st) { BM_copy_if_else<type>(st, b); } \
+  BENCHMARK_REGISTER_F(CopyIfElse, name)                      \
+    ->RangeMultiplier(8)                                      \
+    ->Ranges({{1 << 12, 1 << 27}})                            \
+    ->UseManualTime()                                         \
+    ->Unit(benchmark::kMillisecond);
+
+COPY_BENCHMARK_DEFINE(int16, int16_t, true)
+COPY_BENCHMARK_DEFINE(uint32, uint32_t, true)
+COPY_BENCHMARK_DEFINE(float64, double, true)
+COPY_BENCHMARK_DEFINE(int16_no_nulls, int16_t, false)
+COPY_BENCHMARK_DEFINE(uint32_no_nulls, uint32_t, false)
+COPY_BENCHMARK_DEFINE(float64_no_nulls, double, false)

cpp/benchmarks/replace/clamp_benchmark.cpp
@@ -30,7 +30,7 @@ class ReplaceClamp : public cudf::benchmark {
 };
 
 template <typename type>
-static void BM_reduction_scan(benchmark::State& state, bool include_nulls)
+static void BM_clamp(benchmark::State& state, bool include_nulls)
 {
   cudf::size_type const n_rows{(cudf::size_type)state.range(0)};
   auto const dtype = cudf::type_to_id<type>();
@@ -58,15 +58,15 @@ static void BM_reduction_scan(benchmark::State& state, bool include_nulls)
   }
 }
 
-#define CLAMP_BENCHMARK_DEFINE(name, type, nulls)                         \
-  BENCHMARK_DEFINE_F(ReplaceClamp, name)                                  \
-  (::benchmark::State & state) { BM_reduction_scan<type>(state, nulls); } \
-  BENCHMARK_REGISTER_F(ReplaceClamp, name)                                \
-    ->UseManualTime()                                                     \
-    ->Arg(10000)      /* 10k */                                           \
-    ->Arg(100000)     /* 100k */                                          \
-    ->Arg(1000000)    /* 1M */                                            \
-    ->Arg(10000000)   /* 10M */                                           \
+#define CLAMP_BENCHMARK_DEFINE(name, type, nulls)                \
+  BENCHMARK_DEFINE_F(ReplaceClamp, name)                         \
+  (::benchmark::State & state) { BM_clamp<type>(state, nulls); } \
+  BENCHMARK_REGISTER_F(ReplaceClamp, name)                       \
+    ->UseManualTime()                                            \
+    ->Arg(10000)      /* 10k */                                  \
+    ->Arg(100000)     /* 100k */                                 \
+    ->Arg(1000000)    /* 1M */                                   \
+    ->Arg(10000000)   /* 10M */                                  \
     ->Arg(100000000); /* 100M */
 
 CLAMP_BENCHMARK_DEFINE(int8_no_nulls, int8_t, false);

cpp/benchmarks/replace/nans_benchmark.cpp
@@ -1,0 +1,63 @@
+/*
+ * Copyright (c) 2021, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <benchmark/benchmark.h>
+#include <benchmarks/common/generate_benchmark_input.hpp>
+#include <benchmarks/fixture/benchmark_fixture.hpp>
+#include <benchmarks/synchronization/synchronization.hpp>
+
+#include <cudf/column/column.hpp>
+#include <cudf/column/column_view.hpp>
+#include <cudf/replace.hpp>
+#include <cudf/scalar/scalar_factories.hpp>
+#include <cudf/table/table.hpp>
+#include <cudf/types.hpp>
+
+class ReplaceNans : public cudf::benchmark {
+};
+
+template <typename type>
+static void BM_replace_nans(benchmark::State& state, bool include_nulls)
+{
+  cudf::size_type const n_rows{(cudf::size_type)state.range(0)};
+  auto const dtype = cudf::type_to_id<type>();
+  auto const table = create_random_table({dtype}, 1, row_count{n_rows});
+  if (!include_nulls) { table->get_column(0).set_null_mask(rmm::device_buffer{}, 0); }
+  cudf::column_view input(table->view().column(0));
+
+  auto zero = cudf::make_fixed_width_scalar<type>(0);
+
+  for (auto _ : state) {
+    cuda_event_timer timer(state, true);
+    auto result = cudf::replace_nans(input, *zero);
+  }
+}
+
+#define NANS_BENCHMARK_DEFINE(name, type, nulls)                        \
+  BENCHMARK_DEFINE_F(ReplaceNans, name)                                 \
+  (::benchmark::State & state) { BM_replace_nans<type>(state, nulls); } \
+  BENCHMARK_REGISTER_F(ReplaceNans, name)                               \
+    ->UseManualTime()                                                   \
+    ->Arg(10000)      /* 10k */                                         \
+    ->Arg(100000)     /* 100k */                                        \
+    ->Arg(1000000)    /* 1M */                                          \
+    ->Arg(10000000)   /* 10M */                                         \
+    ->Arg(100000000); /* 100M */
+
+NANS_BENCHMARK_DEFINE(float32_nulls, float, true);
+NANS_BENCHMARK_DEFINE(float64_nulls, double, true);
+NANS_BENCHMARK_DEFINE(float32_no_nulls, float, false);
+NANS_BENCHMARK_DEFINE(float64_no_nulls, double, false);

cpp/src/copying/copy.cu
@@ -20,11 +20,13 @@
 #include <cudf/detail/iterator.cuh>
 #include <cudf/detail/nvtx/ranges.hpp>
 #include <cudf/detail/scatter.hpp>
+#include <cudf/scalar/scalar.hpp>
 #include <cudf/strings/detail/copy_if_else.cuh>
 #include <cudf/strings/string_view.cuh>
 #include <cudf/utilities/traits.hpp>
 
 #include <rmm/cuda_stream_view.hpp>
+#include <rmm/device_uvector.hpp>
 
 namespace cudf {
 namespace detail {
@@ -73,28 +75,18 @@ struct copy_if_else_functor_impl<T, std::enable_if_t<is_rep_layout_compatible<T>
     auto const& lhs = *p_lhs;
     auto const& rhs = *p_rhs;
 
-    if (left_nullable) {
-      if (right_nullable) {
-        auto lhs_iter = cudf::detail::make_pair_iterator<T, true>(lhs);
-        auto rhs_iter = cudf::detail::make_pair_iterator<T, true>(rhs);
-        return detail::copy_if_else(
-          true, lhs_iter, lhs_iter + size, rhs_iter, filter, lhs.type(), stream, mr);
-      }
-      auto lhs_iter = cudf::detail::make_pair_iterator<T, true>(lhs);
-      auto rhs_iter = cudf::detail::make_pair_iterator<T, false>(rhs);
-      return detail::copy_if_else(
-        true, lhs_iter, lhs_iter + size, rhs_iter, filter, lhs.type(), stream, mr);
-    }
-    if (right_nullable) {
-      auto lhs_iter = cudf::detail::make_pair_iterator<T, false>(lhs);
-      auto rhs_iter = cudf::detail::make_pair_iterator<T, true>(rhs);
-      return detail::copy_if_else(
-        true, lhs_iter, lhs_iter + size, rhs_iter, filter, lhs.type(), stream, mr);
-    }
-    auto lhs_iter = cudf::detail::make_pair_iterator<T, false>(lhs);
-    auto rhs_iter = cudf::detail::make_pair_iterator<T, false>(rhs);
-    return detail::copy_if_else(
-      false, lhs_iter, lhs_iter + size, rhs_iter, filter, lhs.type(), stream, mr);
+    auto lhs_iter =
+      cudf::detail::make_optional_iterator<T>(lhs, contains_nulls::DYNAMIC{}, left_nullable);
+    auto rhs_iter =
+      cudf::detail::make_optional_iterator<T>(rhs, contains_nulls::DYNAMIC{}, right_nullable);
+    return detail::copy_if_else(left_nullable || right_nullable,
+                                lhs_iter,
+                                lhs_iter + size,
+                                rhs_iter,
+                                filter,
+                                lhs.type(),
+                                stream,
+                                mr);
   }
 };
 
@@ -247,6 +239,38 @@ std::unique_ptr<column> scatter_gather_based_if_else(cudf::scalar const& lhs,
   return scatter_gather_based_if_else(lhs, rhs_col->view(), size, is_left, stream, mr);
 }
 
+template <>
+struct copy_if_else_functor_impl<struct_view> {
+  template <typename Left, typename Right, typename Filter>
+  std::unique_ptr<column> operator()(Left const& lhs,
+                                     Right const& rhs,
+                                     size_type size,
+                                     bool,
+                                     bool,
+                                     Filter filter,
+                                     rmm::cuda_stream_view stream,
+                                     rmm::mr::device_memory_resource* mr)
+  {
+    return scatter_gather_based_if_else(lhs, rhs, size, filter, stream, mr);
+  }
+};
+
+template <>
+struct copy_if_else_functor_impl<list_view> {
+  template <typename Left, typename Right, typename Filter>
+  std::unique_ptr<column> operator()(Left const& lhs,
+                                     Right const& rhs,
+                                     size_type size,
+                                     bool,
+                                     bool,
+                                     Filter filter,
+                                     rmm::cuda_stream_view stream,
+                                     rmm::mr::device_memory_resource* mr)
+  {
+    return scatter_gather_based_if_else(lhs, rhs, size, filter, stream, mr);
+  }
+};
+
 /**
  * @brief Functor called by the `type_dispatcher` to invoke copy_if_else on combinations
  *        of column_view and scalar
@@ -262,12 +286,6 @@ struct copy_if_else_functor {
                                      rmm::cuda_stream_view stream,
                                      rmm::mr::device_memory_resource* mr)
   {
-    if constexpr (std::is_same_v<T, cudf::list_view> or std::is_same_v<T, cudf::struct_view>) {
-      (void)left_nullable;
-      (void)right_nullable;
-      return scatter_gather_based_if_else(lhs, rhs, size, filter, stream, mr);
-    }
-
     copy_if_else_functor_impl<T> copier{};
     return copier(lhs, rhs, size, left_nullable, right_nullable, filter, stream, mr);
   }
@@ -292,35 +310,21 @@ std::unique_ptr<column> copy_if_else(Left const& lhs,
   auto bool_mask_device_p             = column_device_view::create(boolean_mask);
   column_device_view bool_mask_device = *bool_mask_device_p;
 
-  if (boolean_mask.has_nulls()) {
-    auto filter = [bool_mask_device] __device__(cudf::size_type i) {
-      return bool_mask_device.is_valid_nocheck(i) and bool_mask_device.element<bool>(i);
-    };
-    return cudf::type_dispatcher<dispatch_storage_type>(lhs.type(),
-                                                        copy_if_else_functor{},
-                                                        lhs,
-                                                        rhs,
-                                                        boolean_mask.size(),
-                                                        left_nullable,
-                                                        right_nullable,
-                                                        filter,
-                                                        stream,
-                                                        mr);
-  } else {
-    auto filter = [bool_mask_device] __device__(cudf::size_type i) {
-      return bool_mask_device.element<bool>(i);
-    };
-    return cudf::type_dispatcher<dispatch_storage_type>(lhs.type(),
-                                                        copy_if_else_functor{},
-                                                        lhs,
-                                                        rhs,
-                                                        boolean_mask.size(),
-                                                        left_nullable,
-                                                        right_nullable,
-                                                        filter,
-                                                        stream,
-                                                        mr);
-  }
+  auto const has_nulls = boolean_mask.has_nulls();
+  auto filter          = [bool_mask_device, has_nulls] __device__(cudf::size_type i) {
+    return (!has_nulls || bool_mask_device.is_valid_nocheck(i)) and
+           bool_mask_device.element<bool>(i);
+  };
+  return cudf::type_dispatcher<dispatch_storage_type>(lhs.type(),
+                                                      copy_if_else_functor{},
+                                                      lhs,
+                                                      rhs,
+                                                      boolean_mask.size(),
+                                                      left_nullable,
+                                                      right_nullable,
+                                                      filter,
+                                                      stream,
+                                                      mr);
 }
 
 };  // namespace

cpp/src/copying/segmented_shift.cu
@@ -75,30 +75,20 @@ struct segmented_shift_functor<T, std::enable_if_t<is_rep_layout_compatible<T>()
                                      rmm::mr::device_memory_resource* mr)
   {
     auto values_device_view = column_device_view::create(segmented_values, stream);
-    auto fill_pair_iterator = make_pair_iterator<T>(fill_value);
     bool nullable           = not fill_value.is_valid() or segmented_values.nullable();
-
-    if (segmented_values.has_nulls()) {
-      auto input_pair_iterator = make_pair_iterator<T, true>(*values_device_view) - offset;
-      return copy_if_else(nullable,
-                          input_pair_iterator,
-                          input_pair_iterator + segmented_values.size(),
-                          fill_pair_iterator,
-                          segmented_shift_filter{segment_offsets, offset},
-                          segmented_values.type(),
-                          stream,
-                          mr);
-    } else {
-      auto input_pair_iterator = make_pair_iterator<T, false>(*values_device_view) - offset;
-      return copy_if_else(nullable,
-                          input_pair_iterator,
-                          input_pair_iterator + segmented_values.size(),
-                          fill_pair_iterator,
-                          segmented_shift_filter{segment_offsets, offset},
-                          segmented_values.type(),
-                          stream,
-                          mr);
-    }
+    auto input_iterator =
+      cudf::detail::make_optional_iterator<T>(
+        *values_device_view, contains_nulls::DYNAMIC{}, segmented_values.has_nulls()) -
+      offset;
+    auto fill_iterator = cudf::detail::make_optional_iterator<T>(fill_value, contains_nulls::YES{});
+    return copy_if_else(nullable,
+                        input_iterator,
+                        input_iterator + segmented_values.size(),
+                        fill_iterator,
+                        segmented_shift_filter{segment_offsets, offset},
+                        segmented_values.type(),
+                        stream,
+                        mr);
   }
 };
 

cpp/src/dictionary/replace.cu
@@ -58,17 +58,14 @@ std::unique_ptr<column> replace_indices(column_view const& input,
   auto const d_input    = *input_view;
   auto predicate        = [d_input] __device__(auto i) { return d_input.is_valid(i); };
 
-  using Element = typename thrust::
-    tuple_element<0, typename thrust::iterator_traits<ReplacementIter>::value_type>::type;
-
-  auto input_pair_iterator = cudf::detail::indexalator_factory::make_input_pair_iterator(input);
+  auto input_iterator = cudf::detail::indexalator_factory::make_input_optional_iterator(input);
 
   return cudf::detail::copy_if_else(true,
-                                    input_pair_iterator,
-                                    input_pair_iterator + input.size(),
+                                    input_iterator,
+                                    input_iterator + input.size(),
                                     replacement_iter,
                                     predicate,
-                                    data_type{type_to_id<Element>()},
+                                    data_type{type_to_id<size_type>()},
                                     stream,
                                     mr);
 }
@@ -100,7 +97,7 @@ std::unique_ptr<column> replace_nulls(dictionary_column_view const& input,
 
   auto new_indices =
     replace_indices(input_indices,
-                    cudf::detail::indexalator_factory::make_input_pair_iterator(repl_indices),
+                    cudf::detail::indexalator_factory::make_input_optional_iterator(repl_indices),
                     stream,
                     mr);
 
@@ -133,7 +130,7 @@ std::unique_ptr<column> replace_nulls(dictionary_column_view const& input,
   auto const input_indices = input_view.get_indices_annotated();
   auto new_indices =
     replace_indices(input_indices,
-                    cudf::detail::indexalator_factory::make_input_pair_iterator(*scalar_index),
+                    cudf::detail::indexalator_factory::make_input_optional_iterator(*scalar_index),
                     stream,
                     mr);
   new_indices->set_null_mask(rmm::device_buffer{0, stream, mr}, 0);

commit 2a35ef914e7c266d347338a2f6aa2c4f3a6a191c

Fix memcheck error in copy-if-else (#9467)
CUDA memcheck write error was introduced in #9324 (by me). The `has_nulls` handling had been reworked but a logic error allowed writing past the end of the output column. 

This PR also includes fixes to remove compile warnings in `group_correlation.cu` that only start to appear in nvcc 11.4.

Authors:
  - David Wendt (https://github.com/davidwendt)

Approvers:
  - https://github.com/nvdbaranec
  - Nghia Truong (https://github.com/ttnghia)

URL: https://github.com/rapidsai/cudf/pull/9467

cpp/include/cudf/detail/copy_if_else.cuh
@@ -66,7 +66,7 @@ __launch_bounds__(block_size) __global__
   while (warp_cur <= warp_end) {
     auto const opt_value =
       (index < end) ? (filter(index) ? lhs[index] : rhs[index]) : thrust::nullopt;
-    if (not has_nulls or opt_value) { out.element<T>(index) = static_cast<T>(*opt_value); }
+    if (opt_value) { out.element<T>(index) = static_cast<T>(*opt_value); }
 
     // update validity
     if (has_nulls) {

cpp/src/groupby/sort/group_correlation.cu
@@ -63,6 +63,8 @@ struct type_casted_accessor {
     if constexpr (column_device_view::has_element_accessor<Element>() and
                   std::is_convertible_v<Element, CastType>)
       return static_cast<CastType>(col.element<Element>(i));
+    (void)i;
+    (void)col;
     return {};
   }
 };