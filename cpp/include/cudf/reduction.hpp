difyrrwrzd/cudf

commit 9453a68fe65a1faf941318fce19529fe101ec41b

Change reductions any/all to return valid values for empty input (#12279)
Changes reduction aggregation-all to return true for empty or all-null column.
Changes reduction aggregation-any to return false for empty or all-null column.
This is consistent with the behavior of [`std::all_of` and `std::any_of`](https://en.cppreference.com/w/cpp/algorithm/all_any_none_of) for empty input.
Additional gtests added for this new behavior.
The overall empty/all-null logic input in `cudf::reduce` was simplified as well. 

Closes #8757

Authors:
  - David Wendt (https://github.com/davidwendt)

Approvers:
  - Karthikeyan (https://github.com/karthikeyann)
  - Bradley Dice (https://github.com/bdice)
  - Nghia Truong (https://github.com/ttnghia)

URL: https://github.com/rapidsai/cudf/pull/12279

cpp/include/cudf/reduction.hpp
@@ -39,12 +39,23 @@ enum class scan_type : bool { INCLUSIVE, EXCLUSIVE };
  * @brief  Computes the reduction of the values in all rows of a column.
  *
  * This function does not detect overflows in reductions.
- * Using a higher precision `data_type` may prevent overflow.
+ * Using a higher precision for output_dtype may prevent overflow.
  * Only `min` and `max` ops are supported for reduction of non-arithmetic
- * types (timestamp, string...).
- * The null values are skipped for the operation.
- * If the column is empty, the member `is_valid()` of the output scalar
- * will contain `false`.
+ * types (e.g. timestamp or string).
+ *
+ * Any null values are skipped for the operation.
+ *
+ * If the column is empty or contains all null entries `col.size()==col.null_count()`,
+ * the output scalar value will be `true` for reduction type `any` and false
+ * for reduction type `all`. For all other reductions, the output scalar
+ * returns with `is_valid()==false`.
+ *
+ * If the input column is an arithmetic type, the output_dtype can be any arithmetic
+ * type. If the input column is a non-arithmetic type (e.g. timestamp or string)
+ * the output_dtype must match the column type. If the reduction type is `any` or
+ * `all`, the output_dtype must be type BOOL8.
+ *
+ * If the reduction fails, the output scalar returns with `is_valid()==false`.
  *
  * @throw cudf::logic_error if reduction is called for non-arithmetic output
  * type and operator other than `min` and `max`.
@@ -53,16 +64,9 @@ enum class scan_type : bool { INCLUSIVE, EXCLUSIVE };
  * @throw cudf::logic_error if `min` or `max` reduction is called and the
  * output type does not match the input column data type.
  * @throw cudf::logic_error if `any` or `all` reduction is called and the
- * output type is not bool8.
+ * output type is not BOOL8.
  * @throw cudf::logic_error if `mean`, `var`, or `std` reduction is called and
  * the output type is not floating point.
- *
- * If the input column has arithmetic type, output_dtype can be any arithmetic
- * type. If the input column has non-arithmetic type, e.g. timestamp or string,
- * the same output type must be specified.
- *
- * If the reduction fails, the member is_valid of the output scalar
- * will contain `false`.
  *
  * @param col Input column view
  * @param agg Aggregation operator applied by the reduction
@@ -77,8 +81,12 @@ std::unique_ptr<scalar> reduce(
   rmm::mr::device_memory_resource* mr = rmm::mr::get_current_device_resource());
 
 /**
- * @brief  Computes the reduction of the values in all rows of a column with an initial value. Only
- * SUM, PRODUCT, MIN, MAX, ANY, and ALL aggregations are supported.
+ * @brief  Computes the reduction of the values in all rows of a column with an initial value
+ *
+ * Only `sum`, `product`, `min`, `max`, `any`, and `all` reductions are supported.
+ *
+ * @throw cudf::logic_error if reduction is not `sum`, `product`, `min`, `max`, `any`, or `all`
+ * and `init` is specified.
  *
  * @param col Input column view
  * @param agg Aggregation operator applied by the reduction

cpp/src/reductions/reductions.cpp
@@ -158,25 +158,30 @@ std::unique_ptr<scalar> reduce(
     CUDF_FAIL(
       "Initial value is only supported for SUM, PRODUCT, MIN, MAX, ANY, and ALL aggregation types");
   }
-  // Returns default scalar if input column is non-valid. In terms of nested columns, we need to
-  // handcraft the default scalar with input column.
+
+  // Returns default scalar if input column is empty or all null
   if (col.size() <= col.null_count()) {
     if (agg.kind == aggregation::TDIGEST || agg.kind == aggregation::MERGE_TDIGEST) {
       return tdigest::detail::make_empty_tdigest_scalar(stream);
     }
-    if (col.type().id() == type_id::EMPTY || col.type() != output_dtype) {
+
+    if (output_dtype.id() == type_id::LIST) {
+      if (col.type() == output_dtype) { return make_empty_scalar_like(col, stream, mr); }
       // Under some circumstance, the output type will become the List of input type,
       // such as: collect_list or collect_set. So, we have to handcraft the default scalar.
-      if (output_dtype.id() == type_id::LIST) {
-        auto scalar = make_list_scalar(empty_like(col)->view(), stream, mr);
-        scalar->set_valid_async(false, stream);
-        return scalar;
-      }
-
-      return make_default_constructed_scalar(output_dtype, stream, mr);
+      auto scalar = make_list_scalar(empty_like(col)->view(), stream, mr);
+      scalar->set_valid_async(false, stream);
+      return scalar;
     }
+    if (output_dtype.id() == type_id::STRUCT) { return make_empty_scalar_like(col, stream, mr); }
 
-    return make_empty_scalar_like(col, stream, mr);
+    auto result = make_default_constructed_scalar(output_dtype, stream, mr);
+    if (agg.kind == aggregation::ANY || agg.kind == aggregation::ALL) {
+      // empty input should return true for ANY and return false for ALL
+      dynamic_cast<numeric_scalar<bool>*>(result.get())
+        ->set_value(agg.kind == aggregation::ANY, stream);
+    }
+    return result;
   }
 
   return aggregation_dispatcher(

cpp/tests/reductions/reduction_tests.cpp
@@ -862,11 +862,11 @@ TEST_F(ReductionDtypeTest, different_precision)
                                                    cudf::data_type(cudf::type_id::INT64));
 }
 
-struct ReductionErrorTest : public cudf::test::BaseFixture {
+struct ReductionEmptyTest : public cudf::test::BaseFixture {
 };
 
 // test case for empty input cases
-TEST_F(ReductionErrorTest, empty_column)
+TEST_F(ReductionEmptyTest, empty_column)
 {
   using T        = int32_t;
   auto statement = [](cudf::column_view const& col) {
@@ -893,8 +893,26 @@ TEST_F(ReductionErrorTest, empty_column)
   std::vector<T> col_data(col_size);
   std::vector<bool> valids(col_size, 0);
 
-  cudf::test::fixed_width_column_wrapper<T> col_empty = construct_null_column(col_data, valids);
-  CUDF_EXPECT_NO_THROW(statement(col_empty));
+  cudf::test::fixed_width_column_wrapper<T> col_nulls = construct_null_column(col_data, valids);
+  CUDF_EXPECT_NO_THROW(statement(col_nulls));
+
+  auto any_agg   = cudf::make_any_aggregation<cudf::reduce_aggregation>();
+  auto all_agg   = cudf::make_all_aggregation<cudf::reduce_aggregation>();
+  auto bool_type = cudf::data_type{cudf::type_id::BOOL8};
+
+  auto result = cudf::reduce(col0, *any_agg, bool_type);
+  EXPECT_EQ(result->is_valid(), true);
+  EXPECT_EQ(dynamic_cast<cudf::numeric_scalar<bool>*>(result.get())->value(), true);
+  result = cudf::reduce(col_nulls, *any_agg, bool_type);
+  EXPECT_EQ(result->is_valid(), true);
+  EXPECT_EQ(dynamic_cast<cudf::numeric_scalar<bool>*>(result.get())->value(), true);
+
+  result = cudf::reduce(col0, *all_agg, bool_type);
+  EXPECT_EQ(result->is_valid(), true);
+  EXPECT_EQ(dynamic_cast<cudf::numeric_scalar<bool>*>(result.get())->value(), false);
+  result = cudf::reduce(col_nulls, *all_agg, bool_type);
+  EXPECT_EQ(result->is_valid(), true);
+  EXPECT_EQ(dynamic_cast<cudf::numeric_scalar<bool>*>(result.get())->value(), false);
 }
 
 // ----------------------------------------------------------------------------

commit 2234d73fbcab890f2c095a0f2d4a0ee970c89de0

Fix reductions any/all return value for empty input (#12374)
Fixes error introduced in #12279 to return the correct boolean value for reductions on empty input for `any(false)` and `all(true)`.
Also fixes incorrectly coded gtest to verify the correct result.
Closes #12373

Authors:
  - David Wendt (https://github.com/davidwendt)

Approvers:
  - Jason Lowe (https://github.com/jlowe)
  - Bradley Dice (https://github.com/bdice)
  - Nghia Truong (https://github.com/ttnghia)

URL: https://github.com/rapidsai/cudf/pull/12374

cpp/include/cudf/reduction.hpp
@@ -46,7 +46,7 @@ enum class scan_type : bool { INCLUSIVE, EXCLUSIVE };
  * Any null values are skipped for the operation.
  *
  * If the column is empty or contains all null entries `col.size()==col.null_count()`,
- * the output scalar value will be `true` for reduction type `any` and false
+ * the output scalar value will be `false` for reduction type `any` and `true`
  * for reduction type `all`. For all other reductions, the output scalar
  * returns with `is_valid()==false`.
  *

cpp/src/reductions/reductions.cpp
@@ -177,9 +177,9 @@ std::unique_ptr<scalar> reduce(
 
     auto result = make_default_constructed_scalar(output_dtype, stream, mr);
     if (agg.kind == aggregation::ANY || agg.kind == aggregation::ALL) {
-      // empty input should return true for ANY and return false for ALL
+      // empty input should return false for ANY and return true for ALL
       dynamic_cast<numeric_scalar<bool>*>(result.get())
-        ->set_value(agg.kind == aggregation::ANY, stream);
+        ->set_value(agg.kind == aggregation::ALL, stream);
     }
     return result;
   }

cpp/tests/reductions/reduction_tests.cpp
@@ -902,17 +902,17 @@ TEST_F(ReductionEmptyTest, empty_column)
 
   auto result = cudf::reduce(col0, *any_agg, bool_type);
   EXPECT_EQ(result->is_valid(), true);
-  EXPECT_EQ(dynamic_cast<cudf::numeric_scalar<bool>*>(result.get())->value(), true);
+  EXPECT_EQ(dynamic_cast<cudf::numeric_scalar<bool>*>(result.get())->value(), false);
   result = cudf::reduce(col_nulls, *any_agg, bool_type);
   EXPECT_EQ(result->is_valid(), true);
-  EXPECT_EQ(dynamic_cast<cudf::numeric_scalar<bool>*>(result.get())->value(), true);
+  EXPECT_EQ(dynamic_cast<cudf::numeric_scalar<bool>*>(result.get())->value(), false);
 
   result = cudf::reduce(col0, *all_agg, bool_type);
   EXPECT_EQ(result->is_valid(), true);
-  EXPECT_EQ(dynamic_cast<cudf::numeric_scalar<bool>*>(result.get())->value(), false);
+  EXPECT_EQ(dynamic_cast<cudf::numeric_scalar<bool>*>(result.get())->value(), true);
   result = cudf::reduce(col_nulls, *all_agg, bool_type);
   EXPECT_EQ(result->is_valid(), true);
-  EXPECT_EQ(dynamic_cast<cudf::numeric_scalar<bool>*>(result.get())->value(), false);
+  EXPECT_EQ(dynamic_cast<cudf::numeric_scalar<bool>*>(result.get())->value(), true);
 }
 
 // ----------------------------------------------------------------------------